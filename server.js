6296afcf9daf5975a51b9b5dc97c0650d62067d3
lelefs <lefinochio@hotmail.com>
2020-07-12 13:31:10 -0300

    server

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./src/app');
 var apiPort = process.env.PORT || 3333;
 
 app.listen(apiPort, function () {
-  console.log('🚀️ Backend is running');
+  console.log('🚀️ Backend is running!');
 });
